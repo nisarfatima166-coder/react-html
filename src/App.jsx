b298028b86170e7457a1ccfe01784bb6b691a7f4
nisarfatima166-coder <nisarfatima166@gmail.com>
2026-03-30 01:20:42 -0700

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,20 @@ import Cheese from "./components/Cheese";
 import Chemistry from "./components/Chemistry";
 import Doc from "./components/Doc";
 import Math from "./components/Math";
+import Resume from "./components/Resume";
 
 export default function App() {
   return (
-    <>
-    
     <div>
-      <Learn/>
-      <Register/>
-      <Form/>
-      <Table/>
-      <Cheese/>
-      <Chemistry/>
-      <Doc/>
-      <Math/>
+      <Learn />
+      <Register />
+      <Form />
+      <Table />
+      <Cheese />
+      <Chemistry />
+      <Doc />
+      <Math />
+      <Resume />
     </div>
-
-    </>
-  )
+  );
 }
---
+++ src/components/Resume.jsx
@@ -0,0 +1,98 @@
+export default function Resume() {
+    return (
+        <>
+        
+        <div>
+             <h1>My Resume</h1>
+              <header>
+   
+ <img src="https://images.pexels.com/photos/574077/pexels-photo-574077.jpeg" alt="PROFILE PHOTO" className="profile-pic" style={{ height: "130px", width: "170px" }}/>
+  
+    </header>
+    <li>
+    <h2><u>About Me:</u></h2>
+    
+    <p>
+   <em> <li>Hello! I am a motivated individual with skills in Web Development,
+    Problem-solving, and Teamwork.</li><li>This website shows my Achievements, Education, Skills, 
+    Hobbies, and Contact Details.</li></em>
+  </p>
+   </li>
+   <section id="Education">
+  <h2><u>Education:</u></h2>
+     <ul>
+        <li>
+            <h3>DEGREES:</h3><ul><li><p><em>Matric</em></p></li>
+                            <li><p><em>Intermediate</em></p></li></ul>
+                            
+            
+        </li>
+
+        <li><h3>SCHOOLS:</h3><ul><li><p><em>Govt. Model Girls High School, Canal View, LHR</em></p></li>
+                            <li><p><em>Govt. Post Graduate College, Wahdat Road, LHR</em></p></li></ul>
+            
+        </li>
+        <li><h3>DATES:</h3></li><ul><li><p><em>January 2021 – December 2023</em></p></li>
+        
+        <li><p><em>February 2023 – March 2025</em></p></li></ul>
+        
+    
+     </ul>
+     </section>
+     <section id="Work Experience">
+  <h2><u>Work Experience:</u></h2>
+    <ul>
+         <li><h3>JOB:</h3></li><p><em>Junior Web Developer</em></p>
+    <li><h3>EMPLOYER:</h3><p><em>Arfa Technology Software House</em></p></li>
+    <li><h3>DATES:</h3><p><em>January 2024 – December 2025</em></p></li>
+    
+    <li><h3>RESPONSIBILITIES:</h3>        
+      <ul><li><p><em>Developed and maintained responsive websites using HTML, CSS, and JavaScript.</em></p></li>
+      <li><p><em>Assisted senior developers in debugging and testing web applications.</em></p></li></ul>
+      </li>
+    
+    </ul>
+        </section>
+        <section id="Skills">
+  <h2><u>Skills:</u></h2>
+<ul>
+    <li><p><em>Web Developer</em></p></li>
+    <li><p><em>Full Stack Web Developer</em></p></li>
+    <li><p><em>Full Stack AI Developer</em></p></li>
+    <li><p><em>Software Developer</em></p></li>
+</ul>
+</section>
+<section id="achievements">
+  <h2><u>Awards & Certifications:</u></h2>
+  <ul>
+    <li><p><em>Best Student Award</em></p></li>
+    <li><p><em>Certified Web Developer</em></p></li>
+    <li><p><em>Google Data Analytics Certification</em></p></li>
+    
+  </ul>
+</section>
+ <section id="HOBBIES">
+  <h2><u>Hobbies:</u></h2>
+ <ul>
+    <li><p><em>Reading books</em></p></li>
+    <li><p><em>Sketching</em></p></li>
+    <li><p><em>Photography</em></p></li>
+    <li><p><em>Learning new programming languages</em></p></li>
+  </ul>
+  </section>
+  <section id="Contact details">
+  <h2><u>Contact Details:</u></h2>
+<p>Email: <a href="nisarfatima166@gmail.com">nisarfatima166@gmail.com</a></p>
+  <p>Phone: <a href="tel:+92 3096955756">+92 3096955756</a></p>
+  </section>
+  <footer>
+ <p>©2026 nisar fatima. All rights reserved.</p>
+  <p>
+    Disclaimer: This website is a personal resume created for educational purposes only.
+  </p>
+</footer>
+  </div>
+        
+        </>
+    );
+}
